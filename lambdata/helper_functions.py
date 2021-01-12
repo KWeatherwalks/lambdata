290acf8edabf5051f5847e6351e167af959dd988
Kevin Weatherwalks <kevin.weatherwalks@gmail.com>
2021-01-12 16:43:43 -0500

    Fix helper_functions

--- lambdata/helper_functions.py
+++ lambdata/helper_functions.py
@@ -8,9 +8,10 @@ def null_count(df):
     """Counts number of null values in a Pandas Dataframe"""
     return df.isnull().sum().sum()
 
+
 def train_test_split(df, frac):
     """Splits Pandas Dataframe into two dataframes"""
-    
+
     df = df.copy()
 
     # Number of rows to include in test dataframe
@@ -24,35 +25,40 @@ def train_test_split(df, frac):
     df_test = df.loc[rand_rows]
     df_train = df.drop(labels=rand_rows)
 
+    assert len(df_test)+len(df_train) == len(df), "Size error"
+
     return df_test, df_train
+
 
 def randomize(df, seed):
     """Randomizes the order of a Pandas dataframe"""
     return df.sample(len(df), random_state=seed).reset_index(drop=True)
+
 
 def addy_split(add_series):
     """From a list of addresses, splits each address into city, state, and zip"""
     # TODO - implement
     pass
 
+
 def abbr_2_st(state_series, abbr_2_st=True):
     # TODO - Implement
     pass
+
 
 def list_2_series(list_2_series, df):
     # TODO - Implement
     pass
 
+
 def rm_outlier(df):
     # TODO - Implement
     pass
 
-def split_dates(date_series):
 
+def split_dates(date_series):
     """Creates month, day and year columns from a dataframe"""
-    df = date_series.copy()
-    df['month'] = date_series.str.split('/')[0]
-    df['day'] = date_series.str.split('/')[1]
-    df['year'] = date_series.str.split('/')[2]
+    df = pd.DataFrame()
+    df['year'], df['month'], df['day'] = date_series.str.split('-')
 
     return df
